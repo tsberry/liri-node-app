2333b5e853f1ddeb4f665673297d0c2b230101bb
unknown <tberry6@gmail.com>
2018-04-05 12:45:51 -0700

    Added do-what-it-says (in an unsatisfying way)

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+fs = require("fs");
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
@@ -7,6 +8,16 @@ var request = require("request");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var command = process.argv[2];
+var arg = null;
+if(process.argv.length > 3) arg = process.argv[3];
+
+if (command === "do-what-it-says") {
+    var data = fs.readFileSync("random.txt", "utf8");
+        var arr = data.split(",");
+        command = arr[0];
+        arg = arr[1];
+        console.log(command);
+}
 
 if (command === "my-tweets") {
     client.get("statuses/user_timeline", { user_id: "981941238185127936", count: "20" }, function (error, tweets, response) {
@@ -20,8 +31,8 @@ if (command === "my-tweets") {
 
 if (command === "spotify-this-song") {
     var song;
-    if (process.argv.length < 4) song = "The Sign Ace of Base";
-    else song = process.argv[3];
+    if (arg === null) song = "The Sign Ace of Base";
+    else song = arg;
     spotify.search({ type: "track", query: song, limit: "1" }, function (error, data) {
         if (error) throw error;
         console.log(`${data.tracks.items[0].name} ${data.tracks.items[0].artists[0].name} ${data.tracks.items[0].album.name} ${data.tracks.items[0].preview_url}`);
@@ -30,8 +41,8 @@ if (command === "spotify-this-song") {
 
 if (command === "movie-this") {
     var movie;
-    if (process.argv.length < 4) movie = "Mr. Nobody";
-    else movie = process.argv[3];
+    if (arg === null) movie = "Mr. Nobody";
+    else movie = arg;
     request(`http://www.omdbapi.com/?t=${movie}&apikey=trilogy`, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var info = JSON.parse(body);
@@ -45,8 +56,4 @@ if (command === "movie-this") {
             console.log(info.Actors);
         }
     });
-}
-
-if (command === "do-what-it-says") {
-
 }
